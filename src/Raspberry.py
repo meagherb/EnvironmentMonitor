3a685783edada3a9352e0719b7d7f04190f46b49
soaresb1 <soaresb1@WIT45005639.wit.private>
2019-11-08 15:25:38 -0500

    Created Raspberry.py file and source folder for the code files.

---
+++ src/Raspberry.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 8, 2019
+
+@author: soaresb1
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
